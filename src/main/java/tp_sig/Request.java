8068c06b99950b154a29b7129a7c46fc25469d76
elomedah <elomedah@gmail.com>
2015-01-30 17:11:55 +0100

    code elom

--- src/main/java/tp_sig/Request.java
+++ src/main/java/tp_sig/Request.java
@@ -9,7 +9,6 @@ import org.postgis.Point;
 
 import java.awt.*;
 import java.sql.*;
-import java.util.ArrayList;
 
 public class Request {
 
@@ -63,7 +62,7 @@ public class Request {
     }
 
     /**
-     * Question 11a
+     * Question 11a : Les quartiers de grenoble
      * @param connection
      */
     public static void question11a(Connection connection){
@@ -135,10 +134,6 @@ public class Request {
                 p=g.getPoint(i);
                 drawedPoint = new geoexplorer.gui.Point(p.getX(),p.getY(), Color.blue);
                 drawedLineString.addPoint(drawedPoint);
-                //Print all the points
-                System.out.println("Nom = " + res.getString("highway"));
-                System.out.println("\t\tLongitude = " + p.getX());
-                System.out.println("\t\tLatitude = " + p.getY());
             }
             map.addPrimitive(drawedLineString);
         }
@@ -157,7 +152,7 @@ public class Request {
         st = connection.createStatement();
         //prepareStatement("SELECT linestring, tags->'highway' as highway FROM ways WHERE tags?'highway' LIMIT 1;");
         //result limited to 3 right now
-        res = st.executeQuery("SELECT linestring  FROM ways WHERE tags?'building' AND ST_Intersects(ways.bbox,ST_SetSRID(ST_MakeBox2D(ST_Point(5.7,45.1),ST_Point(5.8,45.2)),4326));");
+        res = st.executeQuery("SELECT linestring, tags?'building' as building FROM ways WHERE tags?'building' AND ST_Intersects(ways.bbox,ST_SetSRID(ST_MakeBox2D(ST_Point(5.7,45.1),ST_Point(5.8,45.2)),4326));");
 
         while (res.next()) {
             Geometry g = ((PGgeometry) res.getObject(1)).getGeometry();
@@ -169,106 +164,6 @@ public class Request {
                 p=g.getPoint(i);
                 drawedPoint = new geoexplorer.gui.Point(p.getX(),p.getY(), Color.blue);
                 drawedPolygon.addPoint(drawedPoint);
-                //Print all the points
-                System.out.println("Nom = " + res.getString("Building"));
-                System.out.println("\t\tLongitude = " + p.getX());
-                System.out.println("\t\tLatitude = " + p.getY());
-            }
-            map.addPrimitive(drawedPolygon);
-        }
-        map.autoAdjust();
-    }
-
-    public static Color getColorAlea(){
-        int red = (int) Math.random()%255;
-        int green =(int) Math.random()%255;
-        int blue = (int) Math.random()%255;
-        Color color = new Color(red,green,blue);
-       return color;
-    }
-    public static void getGrenobleSchool(Connection connection) throws SQLException {
-
-        MapPanel  map = new MapPanel(4.75,44.01,0.1);
-        GeoMainFrame geo = new GeoMainFrame("Map", map);
-
-        st = connection.createStatement();
-        String requete = " SELECT   quartier.the_geom as geom, count(ways.tags->'name') as NbrPharma, quartier.quartier as quartier  " +
-                "FROM ways, quartier WHERE ways.tags->'amenity' like '%school%'" +
-                " and ST_Intersects(ST_Transform(quartier.the_geom,4326)::geometry, ways.linestring) GROUP BY quartier,quartier.the_geom " ;
-
-        res = st.executeQuery(requete);
-
-        while (res.next()) {
-            Geometry g = ((PGgeometry) res.getObject(1)).getGeometry();
-            Point p = null;
-            geoexplorer.gui.Point drawedPoint = null;
-            geoexplorer.gui.Polygon drawedPolygon = new geoexplorer.gui.Polygon(Color.blue,Color.white);
-
-            for (int i = 0; i < g.numPoints(); i++){
-                p=g.getPoint(i);
-                drawedPoint = new geoexplorer.gui.Point(p.getX(),p.getY(), Color.blue);
-                drawedPolygon.addPoint(drawedPoint);
-                //Print all the points
-                System.out.println("Nom = " + res.getString("quartier"));
-                System.out.println("\t\tLongitude = " + p.getX());
-                System.out.println("\t\tLatitude = " + p.getY());
-
-            }
-            map.addPrimitive(drawedPolygon);
-           /* String requetebis = " SELECT  linestring  " +
-                    "FROM ways, quartier WHERE ways.tags->'amenity' like '%school%'" + "AND quartier.quartier like ? " ;
-
-            ResultSet result  ;
-            PreparedStatement state = connection.prepareStatement(requetebis);
-            state.setString(1,res.getString("quartier"));
-            result = state.executeQuery();
-            Boolean continueu = true ;
-            while (result.next() && continueu) {
-                for (int i = 0; i < g.numPoints(); i++) {
-                    p = g.getPoint(i);
-                    drawedPoint = new geoexplorer.gui.Point(p.getX(), p.getY(), Color.blue);
-                    drawedPolygon.addPoint(drawedPoint);
-                    //Print all the points
-                    System.out.println("\t\tLongitude = " + p.getX());
-                    System.out.println("\t\tLatitude = " + p.getY());
-
-                }
-                map.addPrimitive(drawedPolygon);
-                continueu = false;
-            }*/
-
-        }
-        map.autoAdjust();
-    }
-
-    public static void getGrenobleSchoolPerQuartier(Connection connection) throws SQLException {
-
-        MapPanel  map = new MapPanel(4.75,44.01,0.1);
-        GeoMainFrame geo = new GeoMainFrame("Map", map);
-
-        st = connection.createStatement();
-        String requete = " SELECT  ways.linestring   " +
-                "FROM ways, quartier WHERE ways.tags->'amenity' like '%school%' AND quartier.quartier like 'NOTRE DAME' " +
-                " and ST_Intersects(ST_Transform(quartier.the_geom,2154)::geometry, ways.linestring) ";
-
-        PreparedStatement stmt =  connection.prepareStatement(requete);
-
-        res = stmt.executeQuery();
-
-        while (res.next()) {
-            Geometry g = ((PGgeometry) res.getObject(1)).getGeometry();
-            Point p = null;
-            geoexplorer.gui.Point drawedPoint = null;
-            geoexplorer.gui.Polygon drawedPolygon = new geoexplorer.gui.Polygon();
-
-            for (int i = 0; i < g.numPoints(); i++){
-                p=g.getPoint(i);
-                drawedPoint = new geoexplorer.gui.Point(p.getX(),p.getY(), Color.blue);
-                drawedPolygon.addPoint(drawedPoint);
-                //Print all the points
-
-                System.out.println("\t\tLongitude = " + p.getX());
-                System.out.println("\t\tLatitude = " + p.getY());
             }
             map.addPrimitive(drawedPolygon);
         }
@@ -276,16 +171,69 @@ public class Request {
     }
 
     /**
-     * Affiche sur la map une liste de points
-     * @param listPoints
+     * Permet de faire la variation de couleur
+     * @param i
+     * @return
      */
-    public void displayResultsMap(ArrayList<Point> listPoints){
-        Point p;
-        MapPanel map = new MapPanel(4.75,44.01,0.1);
-        GeoMainFrame geo = new GeoMainFrame("Map", map);
-        for(int i = 0; i < listPoints.size();i++){
-            p = listPoints.get(i);
-            map.addPrimitive(new geoexplorer.gui.Point(p.getX(), p.getY(), Color.red));
-        }
+    public static Color getColorAlea(int i){
+        int red = (i*50)%255;
+        int green =( i*10) %255;
+        int blue = ( i*100) %255;
+        Color color = new Color(red,green,blue);
+       return color;
     }
+    public static void getGrenobleSchool(Connection connection) throws SQLException {
+
+        MapPanel  map = new MapPanel(4.75,44.01,0.1);
+        GeoMainFrame geo = new GeoMainFrame("Map", map);
+        int color=1;
+        st = connection.createStatement();
+       String requete = " SELECT   quartier.the_geom as geom, count(ways.tags->'name') as NbrPharma, quartier.quartier as quartier  " +
+                "FROM ways, quartier WHERE ways.tags->'amenity' like '%school%'" +
+                " and ST_Intersects(ST_Transform(quartier.the_geom,4326)::geometry, ways.linestring) GROUP BY quartier,quartier.the_geom " ;
+
+
+        res = st.executeQuery(requete);
+
+        while (res.next()) {
+            Geometry g = ((PGgeometry) res.getObject(1)).getGeometry();
+            Point p = null;
+            geoexplorer.gui.Point drawedPoint = null;
+            geoexplorer.gui.Polygon drawedPolygon = new geoexplorer.gui.Polygon(getColorAlea(color),getColorAlea(color));
+            color++;
+            for (int i = 0; i < g.numPoints(); i++){
+                p=g.getPoint(i);
+                drawedPoint = new geoexplorer.gui.Point(p.getX(),p.getY(), Color.blue);
+                drawedPolygon.addPoint(drawedPoint);
+            }
+            map.addPrimitive(drawedPolygon);
+        }
+        map.autoAdjust();
+
+       /* String requetebis = "  SELECT  linestring  " +
+                "        FROM ways WHERE  ways.tags->'amenity' like '%school%' ;";
+        ResultSet result  ;
+        PreparedStatement state = connection.prepareStatement(requetebis);
+        result = state.executeQuery();
+
+        while (result.next() ) {
+            Geometry g = ((PGgeometry) result.getObject(1)).getGeometry();
+            Point p = null;
+            geoexplorer.gui.Point drawedPoint = null;
+            geoexplorer.gui.LineString drawedLine = new geoexplorer.gui.LineString();
+            for (int i = 0; i < g.numPoints(); i++) {
+                p = g.getPoint(i);
+                drawedPoint = new geoexplorer.gui.Point(p.getX(), p.getY(), Color.blue);
+                drawedLine.addPoint(drawedPoint);
+                //Print all the points
+                System.out.println("\t\tLongitude = " + p.getX());
+                System.out.println("\t\tLatitude = " + p.getY());
+
+            }
+            map.addPrimitive(drawedLine);
+
+        }
+        map.autoAdjust();*/
+    }
+
 }
